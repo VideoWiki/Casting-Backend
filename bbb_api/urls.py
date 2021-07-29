79c966c40a248afd6b7bc74dc70d90549f0b7316
“aman” <amanb1145@gmail.com>
2021-07-29 13:57:04 +0530

    a

--- bbb_api/urls.py
+++ bbb_api/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from .views import create_event, join_meeting, delete_meeting, is_meeting_running, meeting_info, meetings
 
 urlpatterns = [
-    path('meeting/create/', create_event.as_view()),
-    path('meeting/join/', join_meeting.as_view()),
-    path('meetings/', meetings.as_view()),
-    path('meeting/delete/', delete_meeting.as_view()),
-    path('meeting/running/', is_meeting_running.as_view()),
-    path('meeting/info/', meeting_info.as_view()),
+    path('event/meeting/create/', create_event.as_view()),
+    path('event/meeting/join/', join_meeting.as_view()),
+    path('event/meetings/', meetings.as_view()),
+    path('event/meeting/delete/', delete_meeting.as_view()),
+    path('event/meeting/running/', is_meeting_running.as_view()),
+    path('event/meeting/info/', meeting_info.as_view()),
 ]
 
 
