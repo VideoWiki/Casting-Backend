8750b66b2fb732db4dfb385f00123273c78e47a5
“aman” <amanb1145@gmail.com>
2021-07-31 14:18:08 +0530

    updated

--- api/settings.py
+++ api/settings.py
@@ -13,8 +13,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+from .global_variable import DATABASE,Q_C,BASE_DIR,ROLLBAR_ACCESS
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
