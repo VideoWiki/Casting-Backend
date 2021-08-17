c92dfb6a67159330c981e03a40cfb0fc4336be3e
“aman” <amanb1145@gmail.com>
2021-08-17 20:26:51 +0530

    updated

--- bbb_api/get_event_info/event_info_api.py
+++ bbb_api/get_event_info/event_info_api.py
@@ -9,9 +9,21 @@ class meeting_info(APIView):
     def get(self, request):
         public_meeting_id = request.GET.get('public_meeting_id')
         try:
-            private_meeting_id_object = Meeting.objects.get(public_meeting_id=public_meeting_id)
-            result = Meeting.meeting_info(private_meeting_id_object.private_meeting_id, private_meeting_id_object.moderator_password)
-            return Response({'status': True, 'meeting_info': result})
+            event_object = Meeting.objects.get(public_meeting_id= public_meeting_id)
+            # result = Meeting.meeting_info(private_meeting_id_object.private_meeting_id, private_meeting_id_object.moderator_password)
+            event_name = event_object.event_name
+            event_creator_name = event_object.event_creator_name
+            event_creator_email = event_object.event_creator_email
+            description = event_object.short_description
+            event_day = event_object.schedule_time.date()
+            event_time = event_object.schedule_time.time()
+
+            return Response({'status': True, 'meeting_info': {"event_name": event_name,
+                                                              "event_creator_name": event_creator_name,
+                                                              "event_creator_email": event_creator_email,
+                                                              "description": description,
+                                                              "date": event_day,
+                                                              "time": event_time}})
         except ObjectDoesNotExist:
             return Response({"status": False, "message": "meeting id does not exist"},
                             status=status.HTTP_400_BAD_REQUEST)
