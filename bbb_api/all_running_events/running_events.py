eb11aefe41ab7d246e6d27de946e59c7d5ef895c
“aman” <amanb1145@gmail.com>
2021-08-23 18:45:37 +0530

    updated

--- bbb_api/all_running_events/running_events.py
+++ bbb_api/all_running_events/running_events.py
@@ -3,13 +3,18 @@ from ..models import Meeting
 from rest_framework.response import Response
 from ..models import Meeting
 import time
-
+from rest_framework import status
 class meetings(APIView):
     def get(self, request):
         print("here")
         meetings = Meeting.get_meetings()
         if len(meetings) == 0:
-            return Response({'status': False, 'meetings': None, 'message': 'no meeting is running currently'})
+            return Response({'status': False,
+                             'meetings': None,
+                             'message': 'no meeting is running currently'
+                             },
+                            status=status.HTTP_400_BAD_REQUEST
+                            )
         l = []
         for i in meetings:
             name = i['name']
