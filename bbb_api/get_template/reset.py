8dca4e667efd47504f3de1f531ee109c686be8bb
Amanb1145 <amanb1145@gmail.com>
2022-08-13 18:28:05 +0530

    updated templates APIs for private mode

--- bbb_api/get_template/reset.py
+++ bbb_api/get_template/reset.py
@@ -30,46 +30,80 @@ class ResetTemplate(APIView):
             },status=HTTP_400_BAD_REQUEST)
         name = cast_obj.event_name
         schedule_time = cast_obj.schedule_time
-        if role == "co-host":
-            body = f'''You have been invited to join a cast {name} for {schedule_time} 
-                        url for Co-host: {CLIENT_DOMAIN_URL + "/e/{}/?pass={}".format(cast_obj.public_meeting_id, cast_obj.hashed_moderator_password)} 
-                        url for Participant: {CLIENT_DOMAIN_URL + "/e/{}/?pass={}".format(cast_obj.public_meeting_id, cast_obj.hashed_attendee_password)}'''
-            if cast_obj.viewer_mode == True:
-                body_view = f'url for Viewer: {CLIENT_DOMAIN_URL + "/e/{}/?pass={}".format(cast_obj.public_meeting_id, cast_obj.hashed_viewer_password)}'
-                body = body + body_view
-            if cast_obj.is_streaming == True:
-                body_spec = f'url for Spectator: {VW_RTMP_URL + "live/{}".format(cast_obj.public_meeting_id)}'
-                body = body + body_spec
-            template_obj = MailTemplateDetails.objects.get(cast=cast_obj, role=role)
-            template_obj.subject = cast_obj.event_name
-            template_obj.body = body
-            template_obj.save()
-
-        elif role == "participant":
-            body = f'''You have been invited to join a cast {name}, as a Participant. The cast will begin at {schedule_time}
-                        url for cast: {CLIENT_DOMAIN_URL + "/e/{}/?pass={}".format(cast_obj.public_meeting_id, cast_obj.hashed_attendee_password)}'''
-            template_obj = MailTemplateDetails.objects.get(cast=cast_obj, role=role)
-            template_obj.subject = cast_obj.event_name
-            template_obj.body = body
-            template_obj.save()
-
-        if role == "viewer":
-            if viewer_mode == True:
-                body = f'''You have been invited to join a cast {name}, as a Viewer. The cast will begin at {schedule_time}
-                            url for cast: {CLIENT_DOMAIN_URL + "/e/{}/?pass={}".format(cast_obj.public_meeting_id, cast_obj.hashed_viewer_password)}'''
+        if cast_obj.meeting_type == "public":
+            if role == "co-host":
+                body = f'''You have been invited to join a cast {name} for {schedule_time} 
+                            url for Co-host: {CLIENT_DOMAIN_URL + "/e/{}/?pass={}".format(cast_obj.public_meeting_id, cast_obj.hashed_moderator_password)} 
+                            url for Participant: {CLIENT_DOMAIN_URL + "/e/{}/?pass={}".format(cast_obj.public_meeting_id, cast_obj.hashed_attendee_password)}'''
+                if cast_obj.viewer_mode == True:
+                    body_view = f'url for Viewer: {CLIENT_DOMAIN_URL + "/e/{}/?pass={}".format(cast_obj.public_meeting_id, cast_obj.hashed_viewer_password)}'
+                    body = body + body_view
+                if cast_obj.is_streaming == True:
+                    body_spec = f'url for Spectator: {VW_RTMP_URL + "live/{}".format(cast_obj.public_meeting_id)}'
+                    body = body + body_spec
                 template_obj = MailTemplateDetails.objects.get(cast=cast_obj, role=role)
                 template_obj.subject = cast_obj.event_name
                 template_obj.body = body
                 template_obj.save()
 
-        if role == "spectator":
-            if cast_obj.is_streaming == True:
-                body = f'''You have been invited to join a cast {name}, as a Spectator. The cast will begin at {schedule_time}
-                            url for cast: {VW_RTMP_URL + "live/{}".format(cast_obj.public_meeting_id)}'''
+            elif role == "participant":
+                body = f'''You have been invited to join a cast {name}, as a Participant. The cast will begin at {schedule_time}
+                            url for cast: {CLIENT_DOMAIN_URL + "/e/{}/?pass={}".format(cast_obj.public_meeting_id, cast_obj.hashed_attendee_password)}'''
                 template_obj = MailTemplateDetails.objects.get(cast=cast_obj, role=role)
                 template_obj.subject = cast_obj.event_name
                 template_obj.body = body
                 template_obj.save()
+
+            if role == "viewer":
+                if cast_obj.viewer_mode == True:
+                    body = f'''You have been invited to join a cast {name}, as a Viewer. The cast will begin at {schedule_time}
+                                url for cast: {CLIENT_DOMAIN_URL + "/e/{}/?pass={}".format(cast_obj.public_meeting_id, cast_obj.hashed_viewer_password)}'''
+                    template_obj = MailTemplateDetails.objects.get(cast=cast_obj, role=role)
+                    template_obj.subject = cast_obj.event_name
+                    template_obj.body = body
+                    template_obj.save()
+
+            if role == "spectator":
+                if cast_obj.is_streaming == True:
+                    body = f'''You have been invited to join a cast {name}, as a Spectator. The cast will begin at {schedule_time}
+                                url for cast: {VW_RTMP_URL + "live/{}".format(cast_obj.public_meeting_id)}'''
+                    template_obj = MailTemplateDetails.objects.get(cast=cast_obj, role=role)
+                    template_obj.subject = cast_obj.event_name
+                    template_obj.body = body
+                    template_obj.save()
+        elif cast_obj.meeting_type == "private":
+            if role == "co-host":
+                body = f'''You have been invited to join a cast {name}, as a Co-host. The cast will begin at {schedule_time}.
+                            url for cast: {CLIENT_DOMAIN_URL + "/e/{}".format(cast_obj.public_meeting_id)}'''
+                template_obj = MailTemplateDetails.objects.get(cast=cast_obj, role=role)
+                template_obj.subject = cast_obj.event_name
+                template_obj.body = body
+                template_obj.save()
+
+            elif role == "participant":
+                body = f'''You have been invited to join a cast {name}, as a Participant. The cast will begin at {schedule_time}.
+                                        url for cast: {CLIENT_DOMAIN_URL + "/e/{}".format(cast_obj.public_meeting_id)}'''
+                template_obj = MailTemplateDetails.objects.get(cast=cast_obj, role=role)
+                template_obj.subject = cast_obj.event_name
+                template_obj.body = body
+                template_obj.save()
+            if role == "viewer":
+                if cast_obj.viewer_mode == True:
+                    body = f'''You have been invited to join a cast {name}, as a Viewer. The cast will begin at {schedule_time}.
+                                            url for cast: {CLIENT_DOMAIN_URL + "/e/{}".format(cast_obj.public_meeting_id)}'''
+                    template_obj = MailTemplateDetails.objects.get(cast=cast_obj, role=role)
+                    template_obj.subject = cast_obj.event_name
+                    template_obj.body = body
+                    template_obj.save()
+            if role == "spectator":
+                if cast_obj.is_streaming == True:
+                    body = f'''You have been invited to join a cast {name}, as a Spectator. The cast will begin at {schedule_time}.
+                                            url for cast: {VW_RTMP_URL + "live/{}".format(cast_obj.public_meeting_id)}'''
+                    template_obj = MailTemplateDetails.objects.get(cast=cast_obj, role=role)
+                    template_obj.subject = cast_obj.event_name
+                    template_obj.body = body
+                    template_obj.save()
+
 
         return Response({
             "status": True,
