5e30b5fb6d3beabe2ab5ebb40b07bb80f286588d
Amanb1145 <amanb1145@gmail.com>
2023-01-18 15:56:15 +0530

    updated fetch merkel api

--- cast_invitee_details/merkel_tree/save.py
+++ cast_invitee_details/merkel_tree/save.py
@@ -27,10 +27,12 @@ class PostMerkelTreeDetails(APIView):
             MerkelTreeDetail.objects.create(cast=meeting_obj, data=merkel_data)
         else:
             merkel_obj = MerkelTreeDetail.objects.get(cast=meeting_obj)
-            existing_data = merkel_obj.data
-            converted_dict = ast.literal_eval(existing_data)
-            converted_dict.update(merkel_data)
-            merkel_obj.data = converted_dict
+            # existing_data = merkel_obj.data
+            # converted_dict = ast.literal_eval(existing_data)
+            # converted_dict.update(merkel_data)
+            # merkel_obj.data = converted_dict
+            # merkel_obj.save()
+            merkel_obj.data = merkel_data
             merkel_obj.save()
         return Response({
             "status": True,
