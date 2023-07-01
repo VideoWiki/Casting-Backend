af4f80d299bc3066b2b866d73afca305c6ef9788
Aman Bishnoi <amanb1145@gmail.com>
2023-07-01 13:19:30 +0000

    Update models.py

--- td_feedback/models.py
+++ td_feedback/models.py
@@ -8,3 +8,13 @@ class Feedback(models.Model):
     room_id = models.CharField(max_length=100, blank=True)
     feedback_text = models.CharField(max_length=100, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+class UserCount(models.Model):
+    room_id = models.CharField(max_length=50)
+    user_count = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.room_id
+
