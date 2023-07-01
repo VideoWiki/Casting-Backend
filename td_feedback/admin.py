078b656225479e842ce24a54ea230b86df47268d
Aman Bishnoi <amanb1145@gmail.com>
2023-07-01 13:24:54 +0000

    Update admin.py

--- td_feedback/admin.py
+++ td_feedback/admin.py
@@ -1,7 +1,11 @@
-from .models import Feedback
+from .models import Feedback, UserCount
 from django.contrib import admin
 
 
 @admin.register(Feedback)
 class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ('id', )
+
+@admin.register(Feedback)
+class UserCountAdmin(admin.ModelAdmin):
     list_display = ('id', )
