e7845bdcf0316ec1c2ffb1cb1eccc207d49debe2
Aman Bishnoi <amanb1145@gmail.com>
2023-07-01 13:26:21 +0000

    Update urls.py

--- td_feedback/urls.py
+++ td_feedback/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import FeedbackAPIView
+from .user_count import RoomAPIView
 
 urlpatterns = [
     path('feedback/', FeedbackAPIView.as_view(), name='feedback-create'),
+    path('user_count/', RoomAPIView.as_view(), name='feedback-create')
 
 ]
